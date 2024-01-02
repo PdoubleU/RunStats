f73e902a07f9006df5215d334d12249d72efc492
PdoubleU <piotrwitasik1988@gmail.com>
2024-01-02 20:48:32 +0100

    last feature - start and stop session (automatically counted time)

---
+++ RunStats/Migrations/20240102190444_revert-allow-disabled-fields.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RunStats.Migrations
+{
+    public partial class revertallowdisabledfields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubmitDisabledControls",
+                table: "RunningSession");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SubmitDisabledControls",
+                table: "RunningSession",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
